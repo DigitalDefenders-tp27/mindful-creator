1156307794f2b27efef9256f68d2e4790dcaeb3a
Jiahang Sun <jsun0096@student.monash.edu>
2025-05-01 10:06:52 +1000

    Transfer Flask to FastAPI

--- backend/run.py
+++ backend/run.py
@@ -1,10 +1,8 @@
-from app import create_app
+import uvicorn
+from app.main import app
 
-# Create Flask app using factory
-app = create_app()
 
-# No more db.create_all() needed ❌
 
-# Start the Flask server
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000, debug=False)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=False)
